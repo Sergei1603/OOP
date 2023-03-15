0a013272e5729cb00a24f6fc3e86d2b52673feff
Sergei1603 <serega_kuznetsov200@mail.ru>
2023-03-15 21:15:07 +0500

    2

--- ЛР4/Form1.cs
+++ ЛР4/Form1.cs
@@ -1,12 +1,13 @@
 using System.Security.Cryptography.X509Certificates;
-
+using static List;
 namespace ЛР4
 {
     public partial class Form1 : Form
     {
+        MyList<Ccircle> list = new MyList<Ccircle>();
         public class Ccircle
         {
-            public int r = 10;
+            public int r = 50;
             public int x;
             public int y;
             public Ccircle(int x, int y)
@@ -16,9 +17,10 @@ namespace ЛР4
             }
             public void paint_Ccircle(PaintEventArgs e)
             {
-                e.Graphics.DrawEllipse(new Pen(System.Drawing.Color.Black), new Rectangle(x, y, r, r));
+                e.Graphics.FillEllipse(Brushes.Red, x - r/2, y - r/2, r, r);
             }
         }
+//        Ccircle c = new Ccircle(10, 50);
         public Form1()
         {
             InitializeComponent();
@@ -31,13 +33,18 @@ namespace ЛР4
 
         private void pict_box_MouseClick(object sender, MouseEventArgs e)
         {
- //           Ccircle c = new Ccircle(e.X, e.Y);
+            Ccircle c = new Ccircle(e.X, e.Y);
+            list.PushBack(c);
+            pict_box.Refresh();
            
         }
 
         private void pict_box_Paint(object sender, PaintEventArgs e)
         {
-      //      e.Graphics.DrawEllipse(new Pen(System.Drawing.Color.Black), )
+            for (Node<Ccircle> i = list.first; i!= null; i = i.pos)
+            {
+                i.val.paint_Ccircle(e);
+            }
         }
     }
 }
---
+++ ЛР4/MyList.cs
@@ -0,0 +1,165 @@
+﻿	internal class List
+	{
+		public class Node<T>
+		{
+			public Node<T> prev;
+			public Node<T> pos;
+			public T val;
+			public Node(T v)
+			{
+				val = v;
+				prev = null;
+				pos = null;
+				//			Console.WriteLine("Node(T val)");
+			}
+
+		};
+
+		public class MyList<T>
+		{
+			public Node<T> first;
+			public Node<T> last;
+			int size;
+
+			public MyList()
+			{
+				last = null;
+				first = null;
+				size = 0;
+			}
+
+			public void PushBack(T v)
+			{
+				if (size != 0)
+				{
+					Node<T> p = new Node<T>(v);
+					p.prev = last;
+					last.pos = p;
+					last = p;
+				}
+				else
+				{
+					first = new Node<T>(v);
+					last = first;
+				}
+				size++;
+			}
+
+			public void PushFront(T v)
+			{
+				if (size != 0)
+				{
+					Node<T> p = new Node<T>(v);
+					p.pos = first;
+					first.prev = p;
+					first = p;
+				}
+				else
+				{
+					first = new Node<T>(v);
+					last = first;
+				}
+				size++;
+			}
+
+			public void insert(T v, int plase)
+			{
+				if (plase == 0)
+				{
+					PushFront(v);
+				}
+				else if (plase >= size)
+				{
+					PushBack(v);
+				}
+				else
+				{
+					Node<T> cur = first;
+					for (int i = 0; i < plase - 1; i++)
+					{
+						cur = cur.pos;
+					}
+					Node<T> p = new Node<T>(v);
+					Node<T> tmp = cur.pos;
+					cur.pos = p;
+					p.prev = cur;
+
+					tmp.prev = p;
+					p.pos = tmp;
+					size++;
+				}
+			}
+
+			public T pop_front()
+			{
+				T x = this.first.val;
+				first = first.pos;
+				first.prev = null;
+				return x;
+
+			}
+			public T pop_back()
+			{
+				T x = this.last.val;
+				last = last.prev;
+				last.pos = null;
+				return x;
+			}
+			public int get_size()
+			{
+				return size;
+			}
+
+			public int find(T data)
+			{
+				Node<T> cur = first;
+				int i = 0;
+				while (cur != null)
+				{
+					if (cur.val.Equals(data))
+					{
+						return i;
+					}
+					cur = cur.pos;
+					i++;
+				}
+				return -1;
+			}
+			public bool remove(T data)
+			{
+				int f = find(data);
+				if (f == -1)
+				{
+					return false;
+				}
+				else if (f == 0)
+				{
+					first = first.pos;
+					first.prev = null;
+				}
+				else if (f == size - 1)
+				{
+					last = last.prev;
+					last.pos = null;
+				}
+				else
+				{
+					Node<T> cur = first;
+					for (int i = 0; i < f; i++)
+					{
+						cur = cur.pos;
+					}
+					cur.prev.pos = cur.pos;
+					cur.pos.prev = cur.prev;
+				}
+				size--;
+				return true;
+			}
+			public void clear()
+			{
+				first = null;
+				last = null;
+				size = 0;
+			}
+		};
+	}
