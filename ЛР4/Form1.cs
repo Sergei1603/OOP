5998661c4a2d3202c7617356965f08077466becd
Sergei1603 <serega_kuznetsov200@mail.ru>
2023-05-02 23:25:42 +0500

    Revert "изменил лист"
    This reverts commit 3268fd3ba2894c193b67c678898a925bfcc291bf.

--- ЛР4/Form1.cs
+++ ЛР4/Form1.cs
@@ -1,5 +1,6 @@
 using System.Security.Cryptography.X509Certificates;
 using System;
+using static List;
 using System.Drawing;
 using System.Windows.Forms;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement;
--- ЛР4/MyList.cs
+++ ЛР4/MyList.cs
@@ -1,174 +1,190 @@
-﻿        public class Node<T>
+﻿public  class List
+{
+	public class Node<T>
+	{
+		public Node<T> prev;
+		public Node<T> pos;
+		public T val;
+		public Node(T v)
+		{
+			val = v;
+			prev = null;
+			pos = null;
+		}
+
+	};
+
+	public class MyList<T>
+		{
+
+        public Node<T> first;
+			public Node<T> last;
+			int size;
+
+			public MyList()
+			{
+				last = null;
+				first = null;
+				size = 0;
+			}
+        public Iterator<T> CreateIterator()
         {
-            public Node<T> prev;
-            public Node<T> pos;
-            public T val;
-            public Node(T v)
-            {
-                val = v;
-                prev = null;
-                pos = null;
-            }
+            return new Standart_Iterator<T>(this);
+        }
 
-        };
+		public void PushBack(T v)
+		{
+			if (v != null)
+			{
+				if (size != 0)
+				{
+					Node<T> p = new Node<T>(v);
+					p.prev = last;
+					last.pos = p;
+					last = p;
+				}
+				else
+				{
+					first = new Node<T>(v);
+					last = first;
+				}
+				size++;
+			}
+		}
 
-        public class MyList<T>
-        {
-            public Node<T> first;
-            public Node<T> last;
-            int size;
+		public void PushFront(T v)
+		{
+			if (v != null)
+			{
+				if (size != 0)
+				{
+					Node<T> p = new Node<T>(v);
+					p.pos = first;
+					first.prev = p;
+					first = p;
+				}
+				else
+				{
+					first = new Node<T>(v);
+					last = first;
+				}
+				size++;
+			}
+		}
 
-            public MyList()
-            {
-                last = null;
-                first = null;
-                size = 0;
-            }
+			public void insert(T v, int plase)
+			{
+			if (plase == 0)
+			{
+				PushFront(v);
+			}
+			else if (plase >= size)
+			{
+				PushBack(v);
+			}
+			else
+			{
+				if (v != null)
+				{
+					Node<T> cur = first;
+					for (int i = 0; i < plase - 1; i++)
+					{
+						cur = cur.pos;
+					}
+					Node<T> p = new Node<T>(v);
+					Node<T> tmp = cur.pos;
+					cur.pos = p;
+					p.prev = cur;
 
-            public void PushBack(T v)
-            {
-                Node<T> p = new Node<T>(v);
-                p.prev = last;
+					tmp.prev = p;
+					p.pos = tmp;
+					size++;
+				}
+			}
+			}
 
-                if (last != null)
-                    last.pos = p;
-                if (first == null)
-                    first = p;
-                last = p;
-                size++;
-            }
+			public T pop_front()
+			{
+				T x = this.first.val;
+				first = first.pos;
+				first.prev = null;
+				return x;
 
-            public void PushFront(T v)
-            {
-                Node<T> p = new Node<T>(v);
-                p.pos = first;
-                if (first != null)
-                {
-                    first.prev = p;
-                    first = p;
-                }
-                else
-                {
-                    first = p;
-                    last = first;
-                }
-                size++;
-            }
+			}
+			public T pop_back()
+			{
+				T x = this.last.val;
+				last = last.prev;
+				last.pos = null;
+				return x;
+			}
+			public int get_size()
+			{
+				return size;
+			}
 
-            public void insert(T v, int plase)
-            {
-                if (plase <= 0)
-                {
-                    PushFront(v);
-                }
-                else if (plase >= size)
-                {
-                    PushBack(v);
-                }
-                else
-                {
-                    Node<T> cur = first;
-                    for (int i = 0; i < plase - 1; i++)
-                    {
-                        cur = cur.pos;
-                    }
-                    Node<T> p = new Node<T>(v);
-                    Node<T> tmp = cur.pos;
-                    cur.pos = p;
-                    p.prev = cur;
+			public int find(T data)
+			{
+				Node<T> cur = first;
+				int i = 0;
+				while (cur != null)
+				{
+					if (cur.val.Equals(data))
+					{
+						return i;
+					}
+					cur = cur.pos;
+					i++;
+				}
+				return -1;
+			}
+			public bool remove(T data)
+			{
+				int f = find(data);
+				if (f == -1)
+				{
+					return false;
+				}
+				else if (f == 0)
+				{
+					if (size == 1)
+					{
+					first = null;
+					last = null;
+					}
+					else
+					{
+					first = first.pos;
+					first.prev = null;
+					}
+				}
+				else if (f == size - 1)
+				{
+					last = last.prev;
+					last.pos = null;
+				}
+				else
+				{
+					Node<T> cur = first;
+					for (int i = 0; i < f; i++)
+					{
+						cur = cur.pos;
+					}
+					cur.prev.pos = cur.pos;
+					cur.pos.prev = cur.prev;
+				}
+				size--;
+				return true;
+			}
+			public void clear()
+			{
+				first = null;
+				last = null;
+				size = 0;
+			}
+		};
 
-                    tmp.prev = p;
-                    p.pos = tmp;
-                    size++;
-                }
-            }
-
-            public T pop_front()
-            {
-                T pop = first.val;
-                first = first.pos;
-                first.prev = null;
-                return pop;
-
-            }
-            public T pop_back()
-            {
-                T pop = this.last.val;
-                last = last.prev;
-                last.pos = null;
-                return pop;
-            }
-            public int get_size()
-            {
-                return size;
-            }
-
-            public int find(T data)
-            {
-                Node<T> cur = first;
-                int i = 0;
-                while (cur != null)
-                {
-                    if (cur.val.Equals(data))
-                    {
-                        return i;
-                    }
-                    cur = cur.pos;
-                    i++;
-                }
-                return -1;
-            }
-            public bool remove(T data)
-            {
-                int f = find(data);
-                if (f == -1)
-                {
-                    return false;
-                }
-                else if (f == 0)
-                {
-                    if (size == 1)
-                    {
-                        first = null;
-                        last = null;
-                    }
-                    else
-                    {
-                        pop_front();
-                    }
-                }
-                else if (f == size - 1)
-                {
-                    pop_back();
-                }
-                else
-                {
-                    Node<T> cur = first;
-                    for (int i = 0; i < f; i++)
-                    {
-                        cur = cur.pos;
-                    }
-                    cur.prev.pos = cur.pos;
-                    cur.pos.prev = cur.prev;
-                }
-                size--;
-                return true;
-            }
-            public void clear()
-            {
-                first = null;
-                last = null;
-                size = 0;
-            }
-    public Iterator<T> CreateIterator()
-    {
-        return new Standart_Iterator<T>(this);
-    }
-        };
-
-
-
-public abstract class Iterator<T>
+	public abstract class Iterator<T>
 	{
         public MyList<T> list;
         public Node<T> cur_item;
@@ -240,4 +256,4 @@ public abstract class Iterator<T>
 			list.remove(cur_item.val);
         }
     }
-
+	}
